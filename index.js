d974b2448bd678a2b2f32f98cc1dfaa33d0db3ec
DruckDeveloper <druckdev@gmail.com>
2023-02-06 21:42:25 -0500

    backend connection to database established

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config( {path: "./.env"} ); 
 
 const app = express();
 
@@ -13,3 +15,14 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`servidor andando en: ${PORT}`)
 })
+
+const USER = process.env.USER; 
+const PASSWORD = process.env.PASSWORD; 
+const DBNAME = process.env.DBNAME; 
+
+const uri = `mongodb+srv://${USER}:${PASSWORD}@${DBNAME}.swvdyyt.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
