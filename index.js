297c77b67c7e01644ad342cd23770ac90193d0ba
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-02-08 10:54:23 -0500

    conection to db in mongo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 // Create server
 const app = express();
@@ -30,3 +32,13 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`servidor andando en: ${PORT}`)
 })
+
+
+// mongo db conection
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@fundacioncrecer.swvdyyt.mongodb.net/${process.env.DBNAME}`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
