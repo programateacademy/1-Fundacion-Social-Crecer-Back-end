779a3aa4ea0fe0028e72e89036e861319845ab11
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-02-08 18:39:25 -0500

    change bcrypt by bvryptjs1

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Joi = require('@hapi/joi');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 //validation with @joi register
 const schemaRegister = Joi.object({
