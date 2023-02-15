8a06563754fcb083fb7dca9e2e9115cf561eb6f2
DruckDeveloper <druckdev@gmail.com>
2023-02-14 22:44:49 -0500

    Block comment added

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post('/login', async (req, res) => {
 
     // Check if account is locked
     if (userExist.isLocked) {
-        return res.status(401).json({ error: 'Cuenta temporalmente bloqueada. Por favor, intente de nuevo más tarde.' });
+        return res.status(401).json({ error: 'Tu cuenta ha sido bloqueada temporalmente, ponte en contacto con el encargado para desbloquearla' });
     }
 
     //validation password is correct
