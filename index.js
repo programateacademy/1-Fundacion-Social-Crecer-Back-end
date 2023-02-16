e178faf2b57c75b8a5a9a2b5c4b9031155f2d179
DruckDeveloper <druckdev@gmail.com>
2023-02-16 13:00:04 -0500

    change password functionality added

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 // Create server
-require("dotenv").config( {path: "./.env"} ); 
+require("dotenv").config({ path: "./.env" });
 
 //Definicion del servidor
 const app = express();
-app.use (express.json());
+app.use(express.json());
 
 // capture body
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
 // Import routes
 const authRoutes = require('./routes/auth')
 const beneficiariesRoutes = require("./routes/beneficiariesRoutes");
+const changePassword = require('./routes/changePassword')
 
 // Middelwares
 app.use('/api/user', authRoutes)
 
 //Matrix beneficiaries routes
 app.use("/", beneficiariesRoutes);
+
+app.use("/api/user", changePassword);
 
 // Port assign
 const PORT = process.env.PORT || 3001;
@@ -41,5 +44,5 @@ const uri = `${process.env.URL}`;
 mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
-.then(() => console.log('Base de datos conectada'))
-.catch(e => console.log('error db:', e))
+    .then(() => console.log('Base de datos conectada'))
+    .catch(e => console.log('error db:', e))
---
+++ routes/changePassword.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Joi = require('@hapi/joi');
+const User = require('../models/User');
+
+// define the Joi schema for password change request
+const schemaChangePassword = Joi.object({
+    email: Joi.string().min(6).max(1024).required(),
+    newPassword: Joi.string().min(6).max(1024).required(),
+    confirmNewPassword: Joi.string().min(6).max(1024).required(),
+});
+
+// handle the password change request
+router.post('/change-password', async (req, res) => {
+    const { error } = schemaChangePassword.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: 'El usuario no es valido' });
+
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
+
+    const checkPass = await req.body.newPassword === req.body.confirmNewPassword
+    if (!checkPass) {
+        return res.status(400).json({ error: 'Las contraseñas no coinciden' });
+    }
+
+    user.password = hashedPassword;
+    user.isLocked = false;
+    user.failedLoginAttempts = 0;
+    await user.save();
+
+    res.json({ error: null, message: 'La contraseña se ha actualizado correctamente' });
+});
+
+module.exports = router;
